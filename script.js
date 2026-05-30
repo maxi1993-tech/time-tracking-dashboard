f7f20e22f507f8946a39e99727c0376bde6a4773
maxi1993-tech <maxi1993.cm@gmail.com>
2026-05-30 16:24:29 +0200

    feat: add dynamic card generation with config object

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-async function fetchData() {
-  const response = await fetch("./data.json");
-  const data = await response.json();
-  return data;
-}
+const config = {
+  Work: { color: "hsl(15, 100%, 70%)", icon: "./images/icon-work.svg" },
+  Play: { color: "hsl(195, 74%, 62%)", icon: "./images/icon-play.svg" },
+  Study: { color: "hsl(348, 100%, 68%)", icon: "./images/icon-study.svg" },
+  Exercise: { color: "hsl(145, 58%, 55%)", icon: "./images/icon-exercise.svg" },
+  Social: { color: "hsl(264, 64%, 52%)", icon: "./images/icon-social.svg" },
+  "Self Care": {
+    color: "hsl(43, 84%, 65%)",
+    icon: "./images/icon-self-care.svg",
+  },
+};
 
+async function fetchData(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
 async function init() {
-  const data = await fetchData();
+  const data = await fetchData("./data.json");
+  const container = document.querySelector("#cards-container");
+
+  for (let i = 0; i < data.length; i++) {
+    let item = data[i];
+    const current = item.timeframes.weekly.current;
+    const previous = item.timeframes.weekly.previous;
+    const color = config[item.title].color;
+    const icon = config[item.title].icon;
+    const cardHTML = `<article class="card" style="background-color: ${color}"><img src="${icon}" alt=""><div class="card-header"><h2>${item.title}</h2><img src="./images/icon-ellipsis.svg" alt=""></div><div class="card-body"><p>${current}hrs</p><p>Last Week - ${previous}hrs</p></div></article>`;
+    container.innerHTML += cardHTML;
+  }
+
   console.log(data);
 }
 
